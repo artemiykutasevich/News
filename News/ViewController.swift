ea78562f04ee8c874fa5a9300982396c4508b40d
Artem Kutasevich <artemiy.kutasevich@gmail.com>
2022-04-04 10:12:43 +0300

    Initial Commit

---
+++ News/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  News
+//
+//  Created by Artem Kutasevich on 4.04.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
